f672792588e209555d0d5a45c661f69fca43e4d9
Jonathan Bisson <bjonnh-code@bjonnh.net>
2021-03-03 17:56:24 -0600

    [Feature] Add an action to "Make File Executable".

---
+++ src/main/kotlin/net/bjonnh/intellij/filepermissionsplugin/actions/MakeExecutableAction.kt
@@ -0,0 +1,57 @@
+package net.bjonnh.intellij.filepermissionsplugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.attribute.PosixFilePermission
+
+/**
+ * An action that allows a user to make the currently selected file executable
+ */
+class MakeExecutableAction : AnAction() {
+
+    /**
+     * Internal function called by IntelliJ when the users run this action
+     */
+    override fun actionPerformed(e: AnActionEvent) {
+        val currentProject: Project = e.project ?: return
+
+        val path = e.getData(CommonDataKeys.VIRTUAL_FILE)?.canonicalFile?.toNioPath()
+        if (path != null) {
+            try {
+                val permissions = Files.getPosixFilePermissions(path)
+                Files.setPosixFilePermissions(path, permissions + PosixFilePermission.OWNER_EXECUTE)
+            } catch (ex: IOException) {
+                Messages.showMessageDialog(
+                    currentProject,
+                    "${path.fileName} is not accessible.",
+                    "File Access Error",
+                    Messages.getInformationIcon()
+                )
+            }
+        }
+    }
+
+    /**
+     * This action will only be visible in a project and if a file is selected
+     * and it will only be enabled if the file is not executable already
+     */
+    override fun update(e: AnActionEvent) {
+        val project = e.project
+        val path = e.getData(CommonDataKeys.VIRTUAL_FILE)?.canonicalFile?.toNioPath()
+        e.presentation.isEnabled = try {
+            if (path == null) {
+                false
+            } else {
+                PosixFilePermission.OWNER_EXECUTE !in Files.getPosixFilePermissions(path)
+            }
+        } catch (ex: IOException) {
+            false
+        }
+        e.presentation.isVisible = (project != null) && (path != null)
+    }
+}
